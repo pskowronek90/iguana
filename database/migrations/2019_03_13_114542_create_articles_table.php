d86d594fae1a5f4a5b808d133889cf3c36d31db7
Piotr Skowronek <p.skowronek90@gmail.com>
2019-03-13 13:06:19 +0100

    migrations done

---
+++ database/migrations/2019_03_13_114542_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->default(0);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
+            $table->string('title')->default(null);
+            $table->string('content')->default(null);
+            $table->string('img')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
--- database/migrations/2019_03_13_114847_create_comments_table.php
+++ database/migrations/2019_03_13_114847_create_comments_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,11 +12,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedInteger('article_id')->default(0);
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('CASCADE');
+            $table->text('content')->default(null);
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 }
